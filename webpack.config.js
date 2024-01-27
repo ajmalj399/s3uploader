f73df7418fbcfd052b5881ee0bd85beb3fa59704
ajmalj399 <ajmaljamal399@gmail.com>
2024-01-28 01:18:50 +0530

    build lastchanges

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,8 @@ module.exports = {
   resolve: {
     fallback: {
       path: require.resolve("path-browserify"),
-    },
-  },
-};
-const webpack = require("webpack");
-
-module.exports = {
-  resolve: {
-    fallback: {
       os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
     },
   },
 };
